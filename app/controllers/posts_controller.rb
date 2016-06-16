36b98fcfb9896481ffffe4ef48a756cd0aa91149
Hamza Shafiq <hamza.shafiq@square63.com>
2016-06-16 13:14:09 +0500

    sai wala h

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,7 @@
 class PostsController < ApplicationController
   # GET /posts
-  # GET /posts.json 
- 
-  def outoutou
+  # GET /posts.json
+  def index
     @posts = Post.all
 
     respond_to do |format|
